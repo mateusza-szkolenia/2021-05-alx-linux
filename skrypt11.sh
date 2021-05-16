cf3eb847532e43bc635b97cd08da707fb1e0a843
Mateusz Adamowski <m.adamowski@alx.pl>
2021-05-16 14:28:45 +0200

    warunek

---
+++ skrypt11.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Początek skryptu"
+
+slowo="baah"
+
+if grep -q "$slowo" /etc/passwd
+then
+    echo "Znaleziono słowo $slowo w pliku /etc/passwd"
+    echo "hurra!"
+    date
+    echo "koniec"
+fi
+
+echo "Koniec skryptu"
