5034f96eee1d8bab65f363894e96806fd6c17b3a
Mateusz Adamowski <m.adamowski@alx.pl>
2021-05-16 12:18:48 +0200

    parametr $0

---
+++ osemka.sh
@@ -0,0 +1,1 @@
+skrypt08.sh
--- skrypt08.sh
+++ skrypt08.sh
@@ -2,6 +2,8 @@
 
 echo "Liczba parametrów: [$#]"
 
+echo "Zerowy parametr: [$0] nazwa programu/skryptu"
+
 echo "Pierwszy parametr: [$1]"
 echo "Drugi parametr: [$2]"
 echo "Trzeci parametr: [$3]"
