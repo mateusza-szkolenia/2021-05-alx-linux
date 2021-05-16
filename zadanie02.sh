aee525f26724141632deafecd791ba5a36af2dc2
Mateusz Adamowski <m.adamowski@alx.pl>
2021-05-16 12:53:45 +0200

    poprawki cudzysłowów

--- zadanie02.sh
+++ zadanie02.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-format=%F
-data=$(date +$format)
-nazwapliku=backup-doc-$data.tar.gz
+format="%F"
+data="$(date +$format)"
+nazwapliku=backup-doc-"$data".tar.gz
 katalog=/usr/share/doc
 
-tar zcf $nazwapliku $katalog
+tar zcf "$nazwapliku" "$katalog"
 
