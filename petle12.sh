f77f601e3447138e58e15a7381c845ae8935a15a
Mateusz Adamowski <m.adamowski@alx.pl>
2021-05-29 14:51:36 +0200

    czekanie na usunięcie pliku

--- petle12.sh
+++ petle12.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # Napisać skrypt, który w pętli czeka na skasowanie przez użytkownika wskazanego pliku.
+
+while [ -e "stop.txt" ]
+do
+    echo "Wciąż czekam... $(date)"
+    sleep 1
+done
+
+echo "Mogę zaczynać!"
+
+# tutaj jakiś program
